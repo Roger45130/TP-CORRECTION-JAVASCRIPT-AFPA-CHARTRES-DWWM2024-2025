22f0bec1a9fdb0f7ebebba8e30be73921c077458
Roger45130 <rogerlg@netcourrier.com>
2024-12-09 10:13:54 +0100

    Delete 11-LES-ATELIERS-PEINTRES directory

--- 11-LES-ATELIERS-PEINTRES/Scripts/components/footer.js
+++
@@ -1,8 +0,0 @@
-export const Footer = () => {
-
-    return `
-        <footer class="footer">
-            <p class="copyright">&copy; 2024 - Roger-Pierre LE GRASSE</p>
-        </footer>
-    `;
-}
