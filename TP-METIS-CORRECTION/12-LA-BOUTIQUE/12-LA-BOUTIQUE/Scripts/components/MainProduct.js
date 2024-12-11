e679be07c9a93378067d8e5fdd06080befee339b
Roger45130 <rogerlg@netcourrier.com>
2024-12-11 09:38:40 +0100

    Mise à Jour 11122024 0940

---
+++ TP-METIS-CORRECTION/12-LA-BOUTIQUE/12-LA-BOUTIQUE/Scripts/components/MainProduct.js
@@ -0,0 +1,31 @@
+export const MainDetail = (datas) => {
+    const displayArticles = () => {
+        if (!datas || !datas.products) {
+            console.log("Les données des articles sont manquantes ou incorrectes.");
+            return '';
+        }
+
+        return datas.products
+            .map((produit) => {
+                return `
+                    <figure class="card">
+                        <img src="./Assets/Images/Products/${produit.image}" alt="${produit.alt}" class="article__picture">
+                        <figcaption class="card__legend">${produit.name}<br><br>
+                        <button class="btnVoirPlus">Voir plus</button></figcaption>
+                    </figure>
+                `;
+            })
+            .join('');
+    };
+
+    return `
+        <main class="main">
+            <section class="presentation">
+              <h2 class="title__h2">Nos produits</h2>
+              <div class="article__content">
+                ${displayArticles()}
+              </div>
+            </section>
+        </main>
+    `;
+};
